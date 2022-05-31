dba3abb9915f8fd86fa64d24a69dc87443e0c304
“erikayanez“ <erikajoam@gmail.com>
2022-05-31 14:28:15 +0200

    Password generator was created

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const lib = document.querySelector("#showMe")
+const passwordGenerator = () => {
+    let result = [];
+    let options = "1234567890ABCDEF";
+    for (let i = 0; i < 6; i++) {
+      result.push(options[Math.floor(Math.random() * options.length)]);
+    }
+    lib.innerHTML = result.join("");
+    return result.join("");
+  };
+
+  for (const item of items) {
+    item.style.backgroundColor = `#${passwordGenerator()}`;
+  }
